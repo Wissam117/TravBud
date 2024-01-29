b7da03a1dae74587fa8c197e95ef3da0bf367cef
Wissam <mwissam11@gmail.com>
2024-01-29 18:47:02 +0500

    Revamping the scrapping process

--- API.py
+++ API.py
@@ -6,81 +6,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import datetime
 
 # Input placeholders
 max_budget = "10000"
 search_destination = "Islamabad"
-no_of_adults = "2"
-no_of_children = "1"
-children_ages = ["10"]  # Add more as per requirement
-start_date = "2024-02-15"  # Format: YYYY-MM-DD
-end_date = "2024-02-16"  # Format: YYYY-MM-DD
+no_of_adults = "5"
+no_of_children = "5"
+no_of_rooms = "5"
+children_ages = ["10", "1", "2", "4", "5"]  # Add more as per requirement
+start_date = "2024-02-29"  # Format: YYYY-MM-DD
+end_date = "2024-03-03"  # Format: YYYY-MM-DD
 
 # Setting up the Chrome WebDriver
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+url = f"https://www.booking.com/searchresults.html?ss={search_destination}&ssne={search_destination}&ssne_untouched={search_destination}&label=gen173nr-1FCAEoggI46AdIM1gEaLUBiAEBmAExuAEYyAEP2AEB6AEB-AECiAIBqAIEuAKr4dytBsACAdICJDBhMWQxNjRjLTczY2YtNDRkYS04MjllLTFhN2U5YzRlMzM3MtgCBeACAQ&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-2762812&dest_type=city&checkin={start_date}&checkout={end_date}&ltfd=%3A%3A5-2024_4-2024%3A%3A&group_adults={no_of_adults}&no_rooms={no_of_rooms}&group_children={no_of_children}"
+#if there are children
+if int(no_of_children)>0 :
+    for i in range(int(no_of_children)):
+        url += f"&age={children_ages[i]}"
+#extra filters
+url+="&nflt=price%3DPKR-min-15000-1%3Breview_score%3D70" #good reviews and budget constraint
+
 # Navigating to Booking.com
-driver.get("https://www.booking.com")
+driver.get(url)
 
-time.sleep(12)  # Adjust sleep time as necessary
-
-# Determine window size
-window_width = driver.get_window_size()["width"]
-window_height = driver.get_window_size()["height"]
-'''
-
-'''
-
-# Wait for the destination field to be available and enter the search destination
-destination_input = WebDriverWait(driver, 50).until(
-    EC.presence_of_element_located((By.ID, "b2indexPage")) #ss
-)
-destination_input.send_keys(search_destination)
-
-# Selecting the start and end date
-date_field = driver.find_element(By.CLASS_NAME, "xp__dates")
-date_field.click()
-# Use the `data-date` attribute to find the correct date elements and click them
-start_date_element = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, f"td[data-date='{start_date}']"))
-)
-start_date_element.click()
-end_date_element = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, f"td[data-date='{end_date}']"))
-)
-end_date_element.click()
-
-# Adjusting the number of adults and children
-guest_field = driver.find_element(By.CLASS_NAME, "xp__guests")
-guest_field.click()
-
-# Code to set the correct number of adults
-adults_minus_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Decrease number of Adults']")
-adults_plus_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Increase number of Adults']")
-# Adjust adults
-for _ in range(int(no_of_adults) - 1):  # Assumes default is 1 adult
-    adults_plus_button.click()
+time.sleep(5)  # Adjust sleep time as necessary
 
 
-children_plus_button = driver.find_element(By.CSS_SELECTOR, "button[aria-label='Increase number of Children']")
-for _ in range(int(no_of_children)):
-    children_plus_button.click()
-   
-    child_age_dropdown = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, "sb-group__field.sb-group-children__field.sb-group__field--select"))
-    )
-    for index, age in enumerate(children_ages):
-        child_age_dropdown[index].find_element(By.TAG_NAME, "select").send_keys(age)
+prices = driver.find_elements(By.CLASS_NAME, "f6431b446c")
+for price in prices:
+    print(price.text)
 
-
-search_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-search_button.click()
-
-# Wait for the page to load and display the top result
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "sr_property_block")))
-top_result = driver.find_element(By.CLASS_NAME, "sr_property_block")
-print(top_result.text)
-
-# Clean up by closing the browser
+time.sleep(200)
 driver.quit()
